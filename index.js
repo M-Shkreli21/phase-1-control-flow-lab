e82836b5626c96619a46c7a608aa4f6fc5664156
Mark Shkreli <m.shkreli96@icloud.com>
2022-10-10 20:04:54 -0400

    commit tree 6c55ff7985531d7e57dd6a4331b9ca5861c5da85

--- index.js
+++ index.js
@@ -1,11 +1,46 @@
-function scuberGreetingForFeet(){
+function scuberGreetingForFeet(value){
   // Write your code here!
+  let result 
+
+  if (value < 400) {
+    result = "This one is on me!";
+  }
+  else if(value > 400 && value < 2000) {
+    result = "That will be twenty bucks.";
+  }
+  else if(value > 2000 && value <2500) {
+    result = "I will gladly take your thirty bucks.";
+  }
+  else if(value > 2500){
+    result = "No can do.";
+  }
+
+  return result
+};
+
+let destination;
+
+function ternaryCheckCity(destination){
+  // Write your code here!
+
+  const destinationCity = destination === "NYC" ? "Ok, sounds good." : "No go.";
+
+  return destinationCity;
 }
 
-function ternaryCheckCity(){
-  // Write your code here!
-}
+let tip;
 
-function switchOnCharmFromTip(){
-  // Write your code here!
+function switchOnCharmFromTip(tip){
+  switch(tip) {
+    case tip = "generous": 
+    return "Thank you so much."
+    break;
+
+    case tip = "not as generous": 
+    return "Thank you."
+    break;
+
+    default: 
+    return "Bye."
+  }
 }
